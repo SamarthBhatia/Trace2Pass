ba6e28568dc7ccbd9d1c1eef320f1ba43cec0f82
Samarth Bhatia <samarthbhatia700@email.com>
2026-04-10 10:27:05 +0000

    fix(evaluation): expand Docker build script to 24 bugs and fix BuildKit flag

--- evaluation/docker-images/build-buggy-images.sh
+++ evaluation/docker-images/build-buggy-images.sh
@@ -5,7 +5,7 @@
 # so the bug is present. These are used for end-to-end pipeline testing.
 #
 # Usage:
-#   ./build-buggy-images.sh              # Build all 6 images
+#   ./build-buggy-images.sh              # Build all 24 images
 #   ./build-buggy-images.sh 115458       # Build single image
 #   ./build-buggy-images.sh --parallel 2 # Build 2 at a time (default)
 
@@ -18,12 +18,33 @@ DOCKERFILE="$SCRIPT_DIR/Dockerfile.llvm-buggy"
 # Format: BUG_ID:COMMIT:DESCRIPTION
 # These are the parent commits of each fix — they still have the bug.
 BUGS=(
+    # --- Original 6 bugs (already tested) ---
     "115458:d6d73ec89e493c69cf24dc3a710d861e2ce08acb:InstCombine mul/sext (Nov 2024)"
     "72831:42cd9aeec286a6928da59dce1134fdced0f0462a:DSE/BasicAA GEP wrap (Nov 2023)"
     "59836:0db88db5d90f8d8a76360725597e0dfb82cc9661:InstCombine zext mul (Jan 2023)"
     "114578:e577f14b670ee2ae6bb717133310b215be4331b3:InstSimplify/InstCombine div (Nov 2024)"
     "122496:16aa400a2780ab21f73722875734440643f276c3:LoopVectorize SIGKILL (Jan 2025)"
     "129244:56cc9299b78042575422229edb4a7ba15999cbb5:SLPVectorizer wrong code (Feb 2025)"
+    # --- New bugs (parent-of-fix commits) ---
+    "76789:55172b7005a6f972272f6d79f2b0d15063bc1b1c:BasicAA/LICM wrong code (Jan 2024)"
+    "70547:29fd9bab2c9d04b90def77151961c02c940b15bb:CaptureTracking/SimplifyCFG (Oct 2023)"
+    "80113:24a804101b67676aa9fa7f1097043ddd9e2ac1b6:BDCE poison flags (Jan 2024)"
+    "94897:add89088fb8de84fdbeb97c0386a831e51d090a1:InstCombine shl constant (Jun 2024)"
+    "124275:35df525fd00c2037ef144189ee818b7d612241ff:ValueTracking KnownBits (Jan 2025)"
+    "63996:1ae72c0f666beea11de7e9dea17675a89437849c:CodeGen isCopyInstrImpl (Aug 2023)"
+    "64598:2c8b6f16a60e99f5a39c55a91fde747e899c5422:GVN duplicate PHI nodes (Sep 2023)"
+    "85536:f84980570d3f85bdf5c9432647c05bae04a735a0:InstCombine UB attrs speculate (Mar 2024)"
+    "119173:ab77db03ce28e86a61010e51ea13796ea09efc46:LoopVectorize SCEV live-ins (Feb 2025)"
+    "121110:f68dbbbd57dd0947730300d1e827ad16c2dfffb5:VectorCombine shuffle binops (Dec 2024)"
+    "124387:77c325b646301e394bcd89c2980b4c2da8af49cd:InstCombine fshl range attr (Jan 2025)"
+    "115149:099f98c50f8288bd4464acb33779cca9a3cc70c5:InstCombine GEP nowrap (Nov 2024)"
+    "98753:c30ce8b9d33d1050ead549705702c1472b7a7d3f:InstSimplify undef refine (Jul 2024)"
+    "140481:f72a8ee489368dd20c1392b122b0736aa7c8ada1:ConstraintElim overflow (May 2025)"
+    "62992:8d7e90c3b5d22e31c6af25c2f95b8047038590c4:IndVarSimplify FPE (May 2023)"
+    "108698:ba8e4246e2f17030788e8a4954bf5c290332206f:VectorCombine lshr shrink (Sep 2024)"
+    "72855:ae10baf0a0dff53837c3729b8bde64505f54f7aa:MachineLICM CSE on hoist (Nov 2023)"
+    "56103:7c5957aedb75f381cd9996f9eba96f3add16a721:X86 jg/jge SF flag (Jun 2022)"
+    "62175:ff9dc9c4fb113de9619fad6a77f4888277579718:InstCombine mul/shl FPE (Apr 2023)"
 )
 
 MAX_PARALLEL=1
@@ -95,7 +116,7 @@ build_image() {
     tmp_context=$(mktemp -d)
     cp "$DOCKERFILE" "$tmp_context/Dockerfile"
 
-    DOCKER_BUILDKIT=1 docker build \
+    DOCKER_BUILDKIT=0 docker build \
         --build-arg "LLVM_COMMIT=${commit}" \
         --build-arg "BUG_ID=${bug_id}" \
         --build-arg "NINJA_JOBS=${NINJA_JOBS}" \
