76952d0378992d09220c481aba576b5379249eaa
Samarth Bhatia <samarthbhatia700@email.com>
2026-04-27 19:35:46 +0000

    evaluation: +2 bugs llvm-68260 (SCEV/IndVarSimplify) + llvm-63645 (X86 SDAG TargetFrameIndex)

---
+++ evaluation/real-bugs/llvm-63645/test_bug.c
@@ -0,0 +1,42 @@
+// LLVM bug #63645: X86 SDAG TargetFrameIndex aliasing (segfault at -O3)
+// https://github.com/llvm/llvm-project/issues/63645
+// Reporter: shao-hua-li (Csmith fuzzer)
+// Fix commit:    d06608060c8f9d315f98314372110b7b3473f1b3
+// Parent-of-fix: 8fc6b1a18f4d9cc4d481c38bbc503a27acc7e461
+//
+// -O0 output: see issue body.
+// -OX output: differs (the bug).
+
+int c;
+static int *d = &c;
+static int **e = &d;
+static int f = 564255563;
+static int ***g = &e;
+static unsigned j = 4294967295;
+char *l;
+static int *m = &f;
+static int n;
+long p;
+int q(int r) { return r - 10 - 26 + 'a'; }
+void s(int *r) {}
+void t() {
+  int i = n = 0;
+  for (; n <= 1; n++) {
+    int a[2];
+    i = 0;
+    char h[] = {0, 0, 5, 0, 0, 4, 5, 0, 0, 8, 0, 0, 64};
+    char k = *(h + 12);
+    p = k;
+    for (; p + 126 + *m + j - 4859223048 + i < 2; i++)
+      a[q(48 + 4) + j + **e - 112 + i] = 50009;
+    if (0 == 42829 <= a[1])
+      *l = 0;
+  }
+  int ****b[] = {&g, &g};
+}
+int main() {
+  int *o = &c;
+  *o = 0;
+  t();
+  s(&f);
+}
---
+++ evaluation/real-bugs/llvm-68260/test_bug.c
@@ -0,0 +1,41 @@
+// LLVM bug #68260: SCEV invalidate-past-dependency (revert)
+// https://github.com/llvm/llvm-project/issues/68260
+// Reporter: shao-hua-li (Csmith fuzzer)
+// Fix commit:    1c3fdb3d1e187e646f97a305771c48378c5df756
+// Parent-of-fix: 2a2b426f13dfd33c7495da1c54ab9d1a8e625d87
+//
+// -O0 output: see issue body.
+// -OX output: differs (the bug).
+
+int printf(const char *, ...);
+char *a;
+long b, c, d, m;
+int e, f, j, l;
+short g;
+int *k = &j;
+int main() {
+  int *n[] = {&f, &f, &f, &f, &f, &e};
+  d = 0;
+  for (; d <= 1; d++) {
+    g = 0;
+    for (; g <= 4; g++) {
+      char h[] = {0, 0, 4};
+      char *i = h;
+      a = i;
+      do {
+        a++;
+        b /= 10;
+      } while (b);
+      c = a - i;
+      while (i < a){
+        *i = *(i+1);
+        i++;
+      }
+      *k ^= c;
+      k = n[d + g];
+      l ^= m;
+    }
+  }
+  printf("%d\n", e);
+  if (e != 0) __builtin_abort();
+}
