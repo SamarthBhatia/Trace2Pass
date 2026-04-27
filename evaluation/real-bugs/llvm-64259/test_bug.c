23d60156d4314d2a4ecf12e3faf0ea38dd1e7028
Samarth Bhatia <samarthbhatia700@email.com>
2026-04-27 12:49:26 +0000

    evaluation: +1 bug llvm-64669 (InstCombine select+cast); 64259 no_repro (printf-elision oracle mismatch)

---
+++ evaluation/real-bugs/llvm-64259/test_bug.c
@@ -0,0 +1,41 @@
+// LLVM bug #64259: InstCombine irreducible-loop handling
+// https://github.com/llvm/llvm-project/issues/64259
+// Reporter: shao-hua-li (Csmith fuzzer)
+// Fix commit:    72ec2c007e4c5f46995c8f633a946cfa43da6bfb
+// Parent-of-fix: 2cb6d0c70bff616cce4dbd4cbdffc085175c739f
+//
+// -O0 output: see issue body.
+// -OX output: differs (the bug).
+
+int printf(const char *, ...);
+int a, b, d, f, h;
+static int c = 8;
+static int e = 1;
+char g;
+void l(int *m) {
+  if (f) {
+    e = 0;
+    for (;;) {
+      h = 0;
+      for (; h;)
+        ;
+    }
+  }
+  if (a != 0) __builtin_abort();
+}
+int main() {
+  int *i = &c;
+  l(i);
+  if (c)
+  j:
+  k:;
+    else {
+      ;
+      if (e)
+        goto j;
+    }
+  g = b;
+  if (b)
+    goto k;
+  printf("%d\n", a);
+}
---
+++ evaluation/real-bugs/llvm-64669/test_bug.c
@@ -0,0 +1,22 @@
+// LLVM bug #64669: InstCombine select+cast w/ ConstantExpr
+// https://github.com/llvm/llvm-project/issues/64669
+// Reporter: shao-hua-li (Csmith fuzzer)
+// Fix commit:    60a88d4bab72b7c7c5634d58e0b6c08c398991de
+// Parent-of-fix: 1991da9a837dcb083a2a960fbd6a3389da8cc6c1
+//
+// -O0 output: see issue body.
+// -OX output: differs (the bug).
+
+int printf(const char *, ...);
+long a, e;
+int b[72];
+int c, g, h;
+char d;
+int main() {
+  int *f = &b[25], *i = &c;
+  d = f != i;
+  e = a * d;
+  *f = e + d;
+  printf("%d\n", b[25]);
+  if (b[25] != 1) __builtin_abort();
+}
